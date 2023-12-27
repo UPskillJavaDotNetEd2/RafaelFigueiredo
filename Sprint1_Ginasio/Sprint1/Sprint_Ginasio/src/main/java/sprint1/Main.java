6f6d004ff344dd3c978abf75b0566658ba04f269
Rafa <rafa.figueiredo48@gmail.com>
2023-12-27 11:06:37 +0000

    Added exeptions to the Ginasio constructor and removed old code from main

--- Sprint1_Ginasio/Sprint1/Sprint_Ginasio/src/main/java/sprint1/Main.java
+++ Sprint1_Ginasio/Sprint1/Sprint_Ginasio/src/main/java/sprint1/Main.java
@@ -4,78 +4,7 @@ import org.upskill.utils.Data;
 
 public class Main {
     public static void main(String[] args) {
-        /**
-         * Criação de 10 instâncias do tipo Data.
-         */
 
-        Data d1 = new Data(1992, 4, 6);
-        Data d2 = new Data(1990, 12, 16);
-        Data d3 = new Data(1982, 3, 15);
-        Data d4 = new Data(2002, 1, 17);
-        Data d5 = new Data(1999, 2, 3);
-        Data d6 = new Data(1983, 3, 18);
-        Data d7 = new Data(1996, 8, 14);
-        Data d8 = new Data(1955, 4, 1);
-        Data d9 = new Data(1974, 4, 25);
-        Data d10 = new Data(2006, 11, 9);
-
-        /**
-         * Criação de uma instância da classe Ginásio
-         */
-        Ginasio ginasio = new Ginasio("Puxa-Ferro", "Travessa do Ferro, Peso da Regua - Portugal");
-        /**
-         * Criação de 3 objetos do tipo Convidado
-         */
-        Convidado cc1 = new Convidado("David Gomes", "Rua de São Paulo", Genero.MASCULINO, d1, 1.78f, 85, 23, 4, 50, 1, 50, 1000);
-        Convidado cc2 = new Convidado("Paulo Lopes", "Rua de Direita", Genero.MASCULINO, d2, 1.93f, 102, 11, 10, 21, 0, 0, 359);
-        Convidado cc3 = new Convidado("Ana Rita", "Travessa do Desconfiado", Genero.FEMININO, d3, 1.75f, 70, 9, 3, 27, 5, 20, 23);
-        /**
-         * Criação de 3 objetos do tipo Esporadico
-         */
-        Esporadico ce1 = new Esporadico("Carla Sousa", "Avenida Pricipal", Genero.FEMININO, d4, 1.55f, 56, 2, 0, 12);
-        Esporadico ce2 = new Esporadico("João Silva", "Rua Secundária", Genero.MASCULINO, d5, 1.75f, 70, 3, 9, 15);
-        Esporadico ce3 = new Esporadico("Ana Oliveira", "Travessa da Colina", Genero.FEMININO, d5, 1.60f, 65, 4, 6, 10);
-        /**
-         * Criação de 3 objetos do tipo Regular
-         */
-        Regular cr1 = new Regular("Pedro Costa", "Rua Principal", Genero.MASCULINO, d6, 1.80f, 145, 0, 0, 50.0, 0, 0, false);
-        Regular cr2 = new Regular("Marta Santos", "Avenida Central", Genero.FEMININO, d7, 1.65f, 60, 4, 4, 45.0, 18, 10);
-        Regular cr3 = new Regular("Ricardo Oliveira", "Travessa das Flores", Genero.MASCULINO, d8, 1.75f, 70, 0, 0, 35.0, 12, 13.5);
-        /**
-         * Criação de 3 objetos do tipo Treinador
-         */
-        Treinador t1 = new Treinador("Luís Mendes", "Rua dos Desportos", Genero.MASCULINO, d3, 1200.0, 90, 9.0);
-        Treinador t2 = new Treinador("Ana Rocha", "Avenida do Fitness", Genero.FEMININO, d10, 1050.0, 30, 7.5);
-        Treinador t3 = new Treinador("Sérigo Oliveira", "Travessa da Saúde", Genero.MASCULINO, d1, 850.0, 174, 4.5);
-        /**
-         * Criação de 3 objetos do tipo Funcionario
-         */
-        Funcionario f1 = new Funcionario("Miguel Silva", "Rua dos Negócios", Genero.MASCULINO, d2, 10);
-        Funcionario f2 = new Funcionario("Inês Rodrigues", "Avenida Comercial", Genero.FEMININO, d4, 103);
-        Funcionario f3 = new Funcionario("Carlos Pereira", "Travessa das Vendas", Genero.MASCULINO, d5, 0);
-
-        /**
-         * Armazenamento na instância ginasio,dos objetos acima criados.
-         */
-        ginasio.adicionarPessoa(cc1);
-        ginasio.adicionarPessoa(cc2);
-        ginasio.adicionarPessoa(cc3);
-
-        ginasio.adicionarPessoa(cr1);
-        ginasio.adicionarPessoa(cr2);
-        ginasio.adicionarPessoa(cr3);
-
-        ginasio.adicionarPessoa(ce1);
-        ginasio.adicionarPessoa(ce2);
-        ginasio.adicionarPessoa(ce3);
-
-        ginasio.adicionarPessoa(t1);
-        ginasio.adicionarPessoa(t2);
-        ginasio.adicionarPessoa(t3);
-
-        ginasio.adicionarPessoa(f1);
-        ginasio.adicionarPessoa(f2);
-        ginasio.adicionarPessoa(f3);
         /**
          * Impressão da string "#####-:- GINÁSIO -:-####".
          */
